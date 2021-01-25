b2901e4b41b0c80c7c4fbf1aeb444d9722c7c6ab
Danny <ddgame2002@gmail.com>
2021-01-25 18:56:57 +0300

    add and set fullcalendar

---
+++ assets/app.js
@@ -0,0 +1,51 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+// start the Stimulus application
+import './bootstrap';
+
+import { Calendar } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const calendarEl = document.getElementById('calendar');
+    const calendar = new Calendar(calendarEl, {
+        plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
+        headerToolbar: {
+            start: 'prev,next today',
+            center: 'title',
+            end: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+        },
+        themeSystem: 'bootstrap',
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2021-01-01',
+            },
+            {
+                title: 'Long Event',
+                start: '2021-01-07',
+                end: '2021-01-10'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2021-01-09T16:00:00'
+            }
+        ]
+    });
+
+    calendar.render();
+});
---
+++ src/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index_route")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+}
